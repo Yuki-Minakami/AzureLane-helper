1a72c25a357bb724b54955009a5840ebd16e28e4
likaiboy <1140398276@qq.com>
2020-04-09 23:18:00 +0800

    add test

--- rotate2.py
+++ rotate2.py
@@ -12,24 +12,34 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(35, GPIO.OUT, initial=False)
 p = GPIO.PWM(35,50) #50HZ
+GPIO.setup(37, GPIO.OUT, initial=False)
+p2 = GPIO.PWM(37,50) #50HZ
+
 p.start(0)
+p2.start(0)
 sleep(2)
+
 
 def rotate(angle):
     p.ChangeDutyCycle(2+(angle/18))
     sleep(0.5)
     p.ChangeDutyCycle(0)
 
+def click(angle):
+    p2.ChangeDutyCycle(2+(angle/18))
+    sleep(0.5)
+    p2.ChangeDutyCycle(0)
 
-rotate(90)
+
+click(90)
 sleep(1)
-rotate(180)
+click(180)
 sleep(1)
-rotate(90)
-sleep(1)
-rotate(0)
+click(0)
+
 
 p.stop()
+p2.stop()
 GPIO.cleanup()
 
 
