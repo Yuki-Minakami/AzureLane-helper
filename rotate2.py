7bf60b2087c484372ff79f6aff123662ffa21c01
likaiboy <1140398276@qq.com>
2020-04-09 23:06:06 +0800

    add test

--- rotate2.py
+++ rotate2.py
@@ -16,7 +16,6 @@ p.start(0)
 sleep(2)
 
 def rotate(angle):
-    angle = float(input("input angle you want"))
     p.ChangeDutyCycle(2+(angle/18))
     sleep(0.5)
     p.ChangeDutyCycle(0)
@@ -26,6 +25,8 @@ sleep(1)
 rotate(90)
 sleep(1)
 rotate(180)
+sleep(1)
+rotate(0)
 
 p.stop()
 GPIO.cleanup()
