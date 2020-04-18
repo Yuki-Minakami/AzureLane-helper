5dd9c606c3218c2ab9c7d05c9867393776141e1b
likaiboy <1140398276@qq.com>
2020-04-18 23:19:00 +0800

    modify

--- rotate.py
+++ rotate.py
@@ -21,11 +21,12 @@ def rotateA(angle):
     p.ChangeDutyCycle(2+(angle/18))
     sleep(0.4)
     p.ChangeDutyCycle(0)
+    sleep(1)
 
 i=3
 while i>0:
     rotateA(90)
-
+    rotateA(0)
     i =i-1
 
 p.stop()
